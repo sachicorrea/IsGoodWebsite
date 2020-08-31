5b059877619850adb82bda345a1181600ca27a81
sachic <scorrear@tutanota.com>
2020-08-30 23:21:58 -0500

    Including and styling gallery section. Classic WP editor plugin installed

---
+++ wp-content/themes/good_theme/galeria.php
@@ -0,0 +1,72 @@
+<?php
+/*
+Template name: Galeria page
+ */
+
+get_header();
+?>
+	<div class="container">
+		
+		<div class="title">
+			<h1>Galería</h1>
+		</div>
+	
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/TestGood/wp-content/themes/good_theme/images/img2.jpg">
+					<img src="/TestGood/wp-content/themes/good_theme/images/img2.jpg">
+				</a>
+			</div>
+		</div>
+		
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/TestGood/wp-content/themes/good_theme/images/img2.jpg">
+					<img src="/TestGood/wp-content/themes/good_theme/images/img2.jpg">
+				</a>
+			</div>
+		</div>
+
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/TestGood/wp-content/themes/good_theme/images/img2.jpg">
+					<img src="/TestGood/wp-content/themes/good_theme/images/img2.jpg">
+				</a>
+			</div>
+		</div>
+
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/TestGood/wp-content/themes/good_theme/images/img2.jpg">
+					<img src="/TestGood/wp-content/themes/good_theme/images/img2.jpg">
+				</a>
+			</div>
+		</div>
+
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/TestGood/wp-content/themes/good_theme/images/img2.jpg">
+					<img src="/TestGood/wp-content/themes/good_theme/images/img2.jpg">
+				</a>
+			</div>
+		</div>
+
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/TestGood/wp-content/themes/good_theme/images/img2.jpg">
+					<img src="/TestGood/wp-content/themes/good_theme/images/img2.jpg">
+				</a>
+			</div>
+		</div>
+
+		<div class="buttons-container">
+			<button id="button1">Regresar</button>
+			<button id="button2">Continuar</button>
+		</div>
+		
+	</div>
+	
+<?php
+/*get_sidebar();
+get_footer();*/
+?>
